77baa5538a2078ea668bf9f1f64f643c3b14045c
revangelista <r.evangelista@arcanys.com>
2015-09-18 21:38:34 +0800

    Generated new keys

--- decrypt.sh
+++ decrypt.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
-TAR_FILENAME='tmp.tar.gz'
-FOLDER_NAME=$2
+TAR_FILENAME=$2
 SECRET_KEY=secret.key
 RANDOM_KEY=random.key
-SUFFIX='_bak'
-printf "Moving to backup folder.\n"
-mv $FOLDER_NAME $FOLDER_NAME$SUFFIX
 printf "Doing decrytion of random key.\n"
 openssl rsautl -decrypt -inkey $SECRET_KEY -in $RANDOM_KEY.enc -out $RANDOM_KEY
 printf "Doing decryption of $1.\n"
 openssl enc -d -a -aes-256-cbc -in $1 -out $TAR_FILENAME -pass file:$RANDOM_KEY
-printf "Doing extraction.\n"
-tar -xvf $TAR_FILENAME
-printf "Removing $TAR_FILENAME.\n"
-rm -v $TAR_FILENAME
 printf "Done.\n"
--- encrypt.sh
+++ encrypt.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
 # Sample Usage
-# ./encrypt.sh tmp.tar.gz test
+# ./encrypt.sh tmp.tar.gz
 
 TAR_FILENAME=$1
-TARGET=$2
-RANDOM_KEY=secret.key
+RANDOM_KEY=random.key
 PUB_KEY=public.key
 ENC_FILENAME="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)".enc
-rm $TAR_FILENAME
-printf "Doing compression.\n"
-tar -cvf $TAR_FILENAME $TARGET
 printf "Generating random key.\n"
 openssl rand -base64 128 > $RANDOM_KEY
 printf "Done.\n"
@@ -20,7 +16,5 @@ printf "Done.\n"
 printf $ENC_FILENAME" successfully created.\n"
 printf "Encrypting random key.\n"
 openssl rsautl -encrypt -inkey $PUB_KEY -pubin -in $RANDOM_KEY -out $RANDOM_KEY.enc
-printf "Removing $TAR_FILENAME.\n"
-rm -v $TAR_FILENAME
 printf "Done.\n"
 
